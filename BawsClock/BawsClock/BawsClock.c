37fe3584c1cb9b8e7d387e9d19e8f195604e7c16
Morten Toxvaerd <morten@pallas.dk>
2012-03-01 15:27:58 +0100

    Refactored state to structs

--- BawsClock/BawsClock/BawsClock.c
+++ BawsClock/BawsClock/BawsClock.c
@@ -21,7 +21,8 @@
 #define MODE_COUNT		3
 #define MODE_STOPWATCH	1	// Stopwatch: Count from 0
 #define MODE_TIMER		2	// Count down from selected time
-#define MODE_TABATA		3	// 20 sec work, 10 sec pause, 8 rounds
+#define MODE_INTERVAL	3	// x sec work, y sec pause, z rounds
+#define MODE_TABATA		4	// 20 sec work, 10 sec pause, 8 rounds
 
 #define KEY0_MASK		(1 << PD0)
 #define KEY1_MASK		(1 << PD1)
@@ -38,18 +39,162 @@
 #include <util/atomic.h>
 #include <stdbool.h>
 
-volatile bool SecondElapsed  = false;
+// Define structs
+typedef struct {
+	uint8_t digits[4];
+	uint8_t showdigits;
+	uint8_t dots;
+} seven_segment_state;
 
-uint8_t digits[4]  = {0,0,0,0};
-uint8_t showdigits = 0;
-uint8_t dots	   = 0;
+typedef struct {
+	uint8_t Seconds;
+	uint8_t Minutes;
+	uint8_t State;
+	uint8_t Mode;
+	bool CountDown;
+	int PreCount;
+} clock_state;
 
+// Global variables
+seven_segment_state ssState;
+clock_state clockState;
+
+volatile uint8_t SecondElapsed  = false;
 static volatile uint8_t key_press;
 
-ISR(TIMER1_COMPA_vect) {
-	SecondElapsed = true;
+// Function prototypes
+extern double floor(double x);
+uint8_t digitToSevenSegment(uint8_t digit);
+void showDigit(uint8_t digit, uint8_t port);
+
+int main (void) 
+{ 
+	
+	clockState.Seconds    = 0;
+	clockState.Minutes    = 0;
+	clockState.State      = STATE_SELECT;     // Initial state is IDLE
+	clockState.Mode	      = MODE_STOPWATCH;
+	clockState.CountDown  = false;
+	clockState.PreCount   = PRECOUNT;
+	
+	/* SET UP I/O */
+	DDRA = 0xFF;		                   // Enable all port A LEDs
+	DDRB = (1 << PB0) 
+		 | (1 << PB1) 
+		 | (1 << PB2) 
+		 | (1 << PB3);                     // Enable 4 7 segment displays
+	KEY_DDR  = 0;		                   // Set keys as input input
+	KEY_PORT = 0xFF;	                   // Pull-ups on
+
+	/* SET UP TIMERS */
+	// Timer 0: Debouncing 2ms
+	TCCR0 |= (1 << CS00) | (1 << CS02);    // Prescaling 1024
+		
+	// Timer 1: Count seconds
+	TCCR1B |= (1 << WGM12);                // Enable CTC in timer 1's control register
+	OCR1AH = 0x7A;                         // Compare to 31249
+	OCR1AL = 0x11;                         // -||-
+	TCCR1B |= (1 << CS12);                 // Set up timer prescaling at Fcpu/256
+
+	TIMSK |= (1 << OCIE1A) | (1 << TOIE0); // Output Compare Interrup Enable on timer 1 channel A and timer 0
+	
+	sei();
+	for (;;) 
+	{ 
+		switch (clockState.State)
+		{
+		    case STATE_SELECT:
+				if (key_press & KEY0_MASK)
+				{
+					clockState.State = STATE_RUNNING;
+					key_press ^= KEY0_MASK;
+				}
+				if (key_press & KEY1_MASK) {
+					if (++clockState.Mode > MODE_COUNT) clockState.Mode = MODE_STOPWATCH;
+					key_press ^= KEY1_MASK;
+				}
+				if (key_press & KEY2_MASK) {
+					if (--clockState.Mode < 1) clockState.Mode = MODE_COUNT;
+					key_press ^= KEY2_MASK;
+				}
+			
+				ssState.showdigits = (1 << DIGIT0);
+				ssState.digits[DIGIT0] = clockState.Mode;
+				
+				if (SecondElapsed > 0) {
+					ssState.dots ^= (1 << DIGIT0);
+					SecondElapsed--;
+				}
+				break;
+			//case STATE_CONFIGURE:
+				//switch (Mode)
+				//{
+				    //case MODE_STOPWATCH:
+				        //
+				        //break;
+				    //default:
+				        ///* Your code here */
+				        //break;
+				//}
+			case STATE_RUNNING:
+				if (SecondElapsed > 0) {
+					SecondElapsed--;
+					if (clockState.PreCount > 0) {
+						ssState.digits[DIGIT1] = floor(clockState.PreCount / 10);
+						ssState.digits[DIGIT0] = clockState.PreCount % 10;
+						ssState.showdigits = (1 << DIGIT0) | (clockState.PreCount > 9 ? (1 << DIGIT1) : 0);
+						ssState.dots = (clockState.PreCount % 2 == 0 ? (1 << DIGIT0) : 0);
+						clockState.PreCount--;
+					} else {
+						ssState.showdigits = (1 << DIGIT0) | (1 << DIGIT1) | (1 << DIGIT2) | (1 << DIGIT3);
+						ssState.digits[DIGIT3] = floor(clockState.Minutes / 10);
+						ssState.digits[DIGIT2] = clockState.Minutes % 10;
+						ssState.digits[DIGIT1] = floor(clockState.Seconds / 10);
+						ssState.digits[DIGIT0] = clockState.Seconds % 10;
+						if (clockState.Seconds % 2 == 0) ssState.dots = (1 << DIGIT2);
+						else ssState.dots = 0;
+						
+						if (clockState.CountDown) {
+							clockState.Seconds--;
+							if (clockState.Seconds >= 60) {
+								if (clockState.Minutes > 0) { 
+									clockState.Minutes--;
+									clockState.Seconds = 59;
+								} else {
+									clockState.Seconds = 0;
+								}						
+					
+							}
+						}
+						else
+						{
+							clockState.Seconds++;
+							if (clockState.Seconds >= 60) {
+								clockState.Seconds = 0;
+								clockState.Minutes++;
+								if (clockState.Minutes > 59) clockState.Minutes = 0;
+							}
+						}
+					}
+				}
+			default:
+				// Do nothing
+				break;
+		}
+		
+		showDigit(DIGIT3, PB3);
+		showDigit(DIGIT2, PB2);
+		showDigit(DIGIT1, PB1);
+		showDigit(DIGIT0, PB0);
+	} 
 }
 
+// Timer 1 interrupt (1 sec)
+ISR(TIMER1_COMPA_vect) {
+	SecondElapsed++;
+}
+
+// Timer 0 interrupt
 ISR(TIMER0_OVF_vect) {
 	static uint8_t key_state;		// debounced and inverted key state:
 	static uint8_t ct0, ct1;      // holds two bit counter for each key
@@ -101,168 +246,10 @@ uint8_t digitToSevenSegment(uint8_t digit) {
 }
 
 void showDigit(uint8_t digit, uint8_t port) {
-	if (showdigits & (1 << digit)) {
-		PORTA = 0;
+	if (ssState.showdigits & (1 << digit)) {
+		PORTA = 0; // Avoid ghosting
 		PORTB = (1 << port);
-		PORTA = digitToSevenSegment(digits[digit]) | (dots & (1 << digit) ? 1 << PA7 : 0);		
+		PORTA = digitToSevenSegment(ssState.digits[digit]) | (ssState.dots & (1 << digit) ? 1 << PA7 : 0);		
 		_delay_us(50);
 	}
-}
-
-int main (void) 
-{ 
-	uint8_t Seconds    = 0;
-	uint8_t Minutes    = 0;
-	uint8_t State      = STATE_SELECT; // Initial state is IDLE
-	uint8_t Mode	   = MODE_STOPWATCH;
-	bool    CountDown  = false;
-	int     PreCount   = PRECOUNT;
-	
-	/* SET UP I/O */
-	DDRA = 0xFF;		// Enable all port A LEDs
-	DDRB = (1 << PB0) 
-		 | (1 << PB1) 
-		 | (1 << PB2) 
-		 | (1 << PB3);  // Enable 4 7 segment displays
-	KEY_DDR  = 0;		// Set keys as input input
-	KEY_PORT = 0xFF;	// Pull-ups on
-
-	/* SET UP TIMERS */
-	// Timer 0: Debouncing 2ms
-	TCCR0 |= (1 << CS00) | (1 << CS02); // Prescaling 1024
-		
-	// Timer 1: Count seconds
-	TCCR1B |= (1 << WGM12); // Enable CTC in timer 1's control register
-	OCR1AH = 0x7A; // Compare to 31249
-	OCR1AL = 0x11; // -||-
-	TCCR1B |= (1 << CS12); // Set up timer prescaling at Fcpu/256
-
-	TIMSK |= (1 << OCIE1A) | (1 << TOIE0); // Output Compare Interrup Enable on timer 1 channel A and timer 0
-	
-	sei();
-	for (;;) 
-	{ 
-		switch (State)
-		{
-		    case STATE_SELECT:
-				if (key_press & KEY0_MASK)
-				{
-					State = STATE_RUNNING;
-					key_press ^= KEY0_MASK;
-				}
-				if (key_press & KEY1_MASK) {
-					if (++Mode > MODE_COUNT) Mode = MODE_STOPWATCH;
-					key_press ^= KEY1_MASK;
-				}
-				if (key_press & KEY2_MASK) {
-					if (--Mode < 1) Mode = MODE_COUNT;
-					key_press ^= KEY2_MASK;
-				}
-			
-				showdigits = (1 << DIGIT0);
-				digits[DIGIT0] = Mode;
-				
-				if (SecondElapsed) {
-					dots ^= (1 << DIGIT0);
-					SecondElapsed = false;
-				}
-				break;
-			//case STATE_CONFIGURE:
-				//switch (Mode)
-				//{
-				    //case MODE_STOPWATCH:
-				        //
-				        //break;
-				    //default:
-				        ///* Your code here */
-				        //break;
-				//}
-			case STATE_RUNNING:
-				if (SecondElapsed) {
-					SecondElapsed = 0;
-					if (PreCount > 0) {
-						digits[DIGIT1] = floor(PreCount / 10);
-						digits[DIGIT0] = PreCount % 10;
-						showdigits = (1 << DIGIT0) | (PreCount > 9 ? (1 << DIGIT1) : 0);
-						dots = (PreCount % 2 == 0 ? (1 << DIGIT0) : 0);
-						PreCount--;
-					} else {
-						showdigits = (1 << DIGIT0) | (1 << DIGIT1) | (1 << DIGIT2) | (1 << DIGIT3);
-						digits[DIGIT3] = floor(Minutes / 10);
-						digits[DIGIT2] = Minutes % 10;
-						digits[DIGIT1] = floor(Seconds / 10);
-						digits[DIGIT0] = Seconds % 10;
-						if (Seconds % 2 == 0) dots = (1 << DIGIT2);
-						else dots = 0;
-						
-						if (CountDown) {
-							Seconds--;
-							if (Seconds >= 60) {
-								if (Minutes > 0) { 
-									Minutes--;
-									Seconds = 59;
-								} else {
-									Seconds = 0;
-								}						
-					
-							}
-						}
-						else
-						{
-							Seconds++;
-							if (Seconds >= 60) {
-								Seconds = 0;
-								Minutes++;
-								if (Minutes > 59) Minutes = 0;
-							}
-						}
-					}
-				}
-			default:
-				// Do nothing
-				break;
-		}
-		
-		//if (key_press & KEY0_MASK) {
-			//if (CountDown) CountDown = 0;
-			//else CountDown = 1;
-			//key_press = 0;
-		//}
-		//
-		//if (SecondElapsed) {
-			//if (CountDown) {
-				//Seconds--;
-				//if (Seconds >= 60) {
-					//if (Minutes > 0) { 
-						//Minutes--;
-						//Seconds = 59;
-					//} else {
-						//Seconds = 0;
-					//}						
-					//
-				//}
-			//}
-			//else
-			//{
-				//Seconds++;
-				//if (Seconds >= 60) {
-					//Seconds = 0;
-					//Minutes++;
-					//if (Minutes > 59) Minutes = 0;
-				//}
-			//}
-			//SecondElapsed = false;
-			//
-			//
-		//}
-		
-		//unsigned char digit3 = floor(Minutes / 10);
-		//unsigned char digit2 = Minutes % 10;
-		//unsigned char digit1 = floor(Seconds / 10);
-		//unsigned char digit0 = Seconds % 10;
-		showDigit(DIGIT3, PB3);
-		showDigit(DIGIT2, PB2);
-		showDigit(DIGIT1, PB1);
-		showDigit(DIGIT0, PB0);
-	} 
 }
